4fd73e378523922cd77ab0d3a81f7571bcb632b5
weshh20 <125647459+weshh20@users.noreply.github.com>
2023-10-30 02:09:20 -0700

    recursions

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_arrays(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+int _atoi(char *s);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
